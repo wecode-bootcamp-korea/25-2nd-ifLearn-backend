2f816cd3421d0f511ef5fbd310bfb6b1df758947
jh <bndgc@naver.com>
2021-10-18 20:56:29 +0900

    [Mod] modeling

---
+++ carousels/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarouselsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'carousels'
--- ifLearn/settings.py
+++ ifLearn/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'core',
+    'carousels',
+    'courses',
+    'eval',
+    'orders',
+    'users'
 ]
 
 MIDDLEWARE = [
