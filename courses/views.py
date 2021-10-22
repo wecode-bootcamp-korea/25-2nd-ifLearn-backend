480bcf37864add62ebb528a0827dd6615a3df944
KIM <nzlk112@gmail.com>
2021-10-22 12:39:06 +0900

    상철님 요청사항 반영

--- courses/views.py
+++ courses/views.py
@@ -1,18 +1,21 @@
 from django.http    import JsonResponse
 from django.views   import View 
-from courses.models import Category, Course, SubCategory
+from courses.models import Category, Course,SubCategory, CourseHashtag
 
 class CategoryView(View) :
     def get(self,request) :
         subcategory_queryset= SubCategory.objects.values('id','name','category_id')
         category_queryset   = Category.objects.values('name', 'id')
+        hashtag_queryset    = CourseHashtag.objects.select_related('hashtag','course').values('course__subcategory_id','hashtag__name','hashtag__id')
         result              = []
 
         for category in category_queryset :
             result.append({
             category["name"] : [{
                 "id" : sub["id"],
-                "subcategory" : sub["name"]}
+                "subcategory" : sub["name"],
+                "hashtag" : [mid["hashtag__name"] for mid in hashtag_queryset if mid["course__subcategory_id"] == sub["id"]][:12]
+                }
             for sub in subcategory_queryset if sub["category_id"] == category["id"]]
             })
 
@@ -23,6 +26,9 @@ class CourseView(View) :
         
         course_queryset = Course.objects.select_related('level','subcategory', 'subcategory__category', 'sharer'
         ).values('id','name','summary','thumbnail_url','price','level__name', 'subcategory__name', 'subcategory__category__name', 'sharer__nickname')
+        
+        hashtag_queryset    = CourseHashtag.objects.select_related('hashtag','course').values('course__id','hashtag__name','hashtag__id')
+
 
         result          = []
 
@@ -33,14 +39,18 @@ class CourseView(View) :
                 "thumbnail" : course["thumbnail_url"],
                 "title" : course["name"],
                 "author" : course["sharer__nickname"],
-                "price" : course["price"],
+                "price" : int(course["price"]),
                 "summary" : course["summary"],
                 "level" : course["level__name"],
                 "sub-cateogry" : course["subcategory__name"],
                 "category" : course["subcategory__category__name"],
                 "star-number" : 0,
                 "popular" : 0,
-                "hash-tag" : ["빈값1","빈값2","빈값3"]
+                "hash-tag" : [
+                    {"tag_name": tag["hashtag__name"],
+                    "tag_id" : tag["hashtag__id"]
+                    } for tag in hashtag_queryset if tag["course__id"] == course["id"]
+                ]
             })
 
         return JsonResponse({
--- users/models.py
+++ users/models.py
@@ -6,11 +6,11 @@ class User(TimeStampModel) :
     nickname     = models.CharField(max_length = 100)
     password     = models.CharField(max_length = 200)
     email        = models.CharField(max_length = 100)
-    phone_number = models.CharField(max_length = 13, null=True)
-    introduce    = models.TextField(default = '', null=True)
-    blog         = models.CharField(max_length = 100, default = '', null=True)
+    phone_number = models.CharField(max_length = 13, blank=True)
+    introduce    = models.TextField(default = '', blank=True)
+    blog         = models.CharField(max_length = 100, default = '', blank=True)
     sharer       = models.BooleanField(default = False)
-    kakao_id     = models.CharField(max_length = 100)
+    kakao_id     = models.CharField(max_length = 100, default='', blank=True)
 
     class Meta :
         db_table = "users"
