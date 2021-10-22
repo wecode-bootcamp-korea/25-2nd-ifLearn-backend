62b4d765bc40e71e678e3bd9f178fd6188c46100
KIM <nzlk112@gmail.com>
2021-10-22 17:20:45 +0900

    [ADD] Video Player Views and Test
    1. class VideoPlayer
    - HTTP reqeust, 파일경로를 input으로 받아, 경로에 있는 동영상 bytes로 응답
    - 파일이 없으면 No File 204 응답
    - 브라우저에서 동영상을 재생하기 위한 필수값 Header에 전달 (content-type, content-length, accept-ranges)
    - HTTP requeest에 Range가 있으면 해당 부분부터 데이터 응답
    - Range가 없으면 처음부터 응답
    
    2. class RangeFileWrapper
    - 파일을 8kb 단위로 잘라서 iterable한 형태로 변환
    - 남은 용량이 없으면 파일 read중단
    
     커밋할 변경 사항:
    	수정함:        courses/tests.py
    	수정함:        courses/views.py
    	수정함:        ifLearn/settings.py
    	수정함:        ifLearn/urls.py

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+import unittest
+import json
+import re
 
-# Create your tests here.
+from django.http.request import HttpRequest
+from courses.views  import VideoPlayer
+
+class VideoPlayerTest(unittest.TestCase) :
+    test_cls = VideoPlayer()
+    test_req = HttpRequest()
+    
+    def test_get_content_type_noraml(self):
+        path = 'test_video.mp4'
+        self.assertEqual(self.test_cls.get_content_type(path), 'video/mp4')
+    
+    def test_get_content_type_wrong_file(self) :
+        path = 'test_empty_file'
+        self.assertEqual(self.test_cls.get_content_type(path), 'application/octet-stream')
+
+    def test_get_http_range_normal(self) :
+        test_req                    = HttpRequest()
+        test_req.META['HTTP_RANGE'] = 'bytes = 0-150'
+        self.assertEqual(self.test_cls.get_http_range(test_req).group(0), 'bytes = 0-150')
+
+    def test_get_http_range_no_range(self) :
+        self.assertEqual(self.test_cls.get_http_range(self.test_req), None)
+
+    def test_get_range_normal(self) :
+        size = 300
+        range_match = re.match("(\d+)\s(\d*)","0 150")
+        self.assertEqual(self.test_cls.get_range(size,range_match), (0,150))
+
+    def test_get_range_out_of_size(self) :
+        size = 100
+        range_match = re.match("(\d+)\s(\d*)","0 150")
+        self.assertEqual(self.test_cls.get_range(size,range_match), (0,100-1))
+
+    def test_get_normal(self) :
+        path = 'test_video.mp4'
+        self.assertEqual(self.test_cls.get(self.test_req, path).content[:20], b'\x00\x00\x00\x18ftypiso6\x00\x00\x00\x01iso6')
+
+    def test_get_no_file(self) :
+        path = '!Q2w#E4r'
+        # json 끼리 비교가 안되기 대문에 파이썬 데이터로 변경
+        self.assertEqual(json.loads(self.test_cls.get(self.test_req, path).content), {"MEESAGE" : "No file"})
+
+    def test_get_with_range(self) :
+        path = 'test_video.mp4'
+        test_req                    = HttpRequest()
+        test_req.META['HTTP_RANGE'] = 'bytes = 0-150'
+        self.assertEqual(self.test_cls.get(test_req, path).headers["Content-Range"], "bytes 0-150/6165797")
+
+    def test_get_with_range_negative(self) :
+        path = 'test_video.mp4'
+        test_req                    = HttpRequest()
+        test_req.META['HTTP_RANGE'] = 'bytes = -200-150'
+        # 마이너스가 붙으면, regex와 match되지 않아 range를 무시한다.
+        self.assertEqual(self.test_cls.get(self.test_req, path).content[:20], b'\x00\x00\x00\x18ftypiso6\x00\x00\x00\x01iso6')
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+import os
+import re
+import mimetypes
 
-# Create your views here.
+from wsgiref.util   import FileWrapper
+from django.http    import JsonResponse, StreamingHttpResponse
+from django.http.response import HttpResponse
+from django.views   import View 
+
+
+class RangeFileWrapper:
+    def __init__(self, file, blksize=8192, offset=0, length=None):
+        self.file = file
+        self.file.seek(offset, 0)
+        self.remaining = length
+        self.blksize = blksize
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.remaining is None:
+            # If remaining is None, we're reading the entire file.
+            data = self.file.read(self.blksize)
+            if data:
+                return data
+            raise StopIteration()
+        else:
+            if self.remaining <= 0:
+                raise StopIteration()
+            data = self.file.read(min(self.remaining, self.blksize))
+            if not data:
+                raise StopIteration()
+            self.remaining -= len(data)
+            return data
+
+
+class VideoPlayer(View) :    
+    def get_content_type(self,path) :
+        content_type = mimetypes.guess_type(path)[0]
+        content_type = content_type or 'application/octet-stream'
+        
+        return content_type
+
+
+    def get_http_range(self,request) :
+        range_header =request.META.get('HTTP_RANGE', '').strip()
+        # 띄어쓰기를 엄격하게 하지 않기 위해 \s* 을 계속 붙
+        range_match = re.match(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)',range_header, re.I)
+        return range_match
+
+
+    def get_range(self,size,range_match) :
+        first_byte, last_byte = range_match.groups()
+        first_byte  = int(first_byte) if first_byte else 0
+        last_byte   = int(last_byte) if last_byte else size - 1
+        if last_byte >= size:
+            last_byte = size - 1
+        
+        return first_byte, last_byte
+
+
+    def get(self,request,path) :
+        if not os.path.isfile(path) :
+            return JsonResponse({"MEESAGE" : "No file"}, status=204)
+
+        size         = os.path.getsize(path)
+        range_match  = self.get_http_range(request)
+        content_type = self.get_content_type(path)
+        video_file   = open(path, 'rb')
+        
+        if range_match :
+            first_byte, last_byte   = self.get_range(size,range_match)
+            length                  = last_byte - first_byte + 1
+            resp                    = StreamingHttpResponse(RangeFileWrapper(video_file, offset=first_byte, length=length), status=206, content_type=content_type)
+            resp['Content-Length']  = str(length)
+            resp['Content-Range']   = f'bytes {first_byte}-{last_byte}/{size}'    
+        else:
+            resp = HttpResponse(FileWrapper(video_file), content_type=content_type)
+            resp['Content-Length'] = str(size)    
+        
+        resp['Accept-Ranges'] = 'bytes'
+        
+        return resp
+        
