27b4d406bb3c9e1976027d68c3f3e095c1dc896d
KIM <nzlk112@gmail.com>
2021-10-22 12:47:03 +0900

    비디오 플레이어 복붙

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,12 @@
-from django.http    import JsonResponse
+import os
+import re
+import mimetypes
+
+from wsgiref.util   import FileWrapper
+from django.http    import JsonResponse, StreamingHttpResponse
 from django.views   import View 
 from courses.models import Category, Course,SubCategory, CourseHashtag
+
 
 class CategoryView(View) :
     def get(self,request) :
@@ -56,3 +62,75 @@ class CourseView(View) :
         return JsonResponse({
             "result" : result
         })
+
+class RangeFileWrapper:
+    def __init__(self, filelike, blksize=1024*1024, offset=0, length=None):
+        self.filelike = filelike
+        self.filelike.seek(offset, os.SEEK_SET)
+        self.remaining = length
+        self.blksize = blksize
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.remaining is None:
+            # If remaining is None, we're reading the entire file.
+            data = self.filelike.read(self.blksize)
+            if data:
+                return data
+            raise StopIteration()
+        else:
+            if self.remaining <= 0:
+                raise StopIteration()
+            data = self.filelike.read(min(self.remaining, self.blksize))
+            if not data:
+                raise StopIteration()
+            self.remaining -= len(data)
+            return data
+
+
+class VideoPlayer(View) :    
+    def get_content_type(self,path) :
+        content_type = mimetypes.guess_type(path)[0]
+        content_type = content_type or 'application/octet-stream'
+        
+        return content_type
+
+
+    def get_http_range(self,request) :
+        range_header =request.META.get('HTTP_RANGE', '').strip()
+        range_match = re.match(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)',range_header, re.I)
+        return range_match
+
+
+    def get_range(self,size,range_match) :
+        first_byte, last_byte = range_match.groups()
+        first_byte  = int(first_byte) if first_byte else 0
+        last_byte   = int(last_byte) if last_byte else size - 1
+        if last_byte >= size:
+            last_byte = size - 1
+        
+        return first_byte, last_byte
+
+
+    def get(self,request,path) :
+        size         = os.path.getsize(path)
+        range_match  = self.get_http_range(request)
+        content_type = self.get_content_type(path)
+        video_file   = open(path, 'rb')
+        
+        if range_match :
+            first_byte, last_byte   = self.get_range(size,range_match)
+            length                  = last_byte - first_byte + 1
+            resp                    = StreamingHttpResponse(RangeFileWrapper(video_file, offset=first_byte, length=length), status=206, content_type=content_type)
+            resp['Content-Length']  = str(length)
+            resp['Content-Range']   = f'bytes {first_byte}-{last_byte}/{size}'    
+        else:
+            resp = StreamingHttpResponse(FileWrapper(video_file), content_type=content_type)
+            resp['Content-Length'] = str(size)    
+        
+        resp['Accept-Ranges'] = 'bytes'
+        
+        return resp
+        
