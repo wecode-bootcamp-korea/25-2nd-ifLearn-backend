90da6850b92cd69ffd6fbe3c984585e116837af6
KIM <km0192@naver.com>
2021-10-26 21:42:02 +0900

    [ADD] Video Player Views and Test
    1. class VideoPlayer
    - HTTP reqeust, 파일경로를 input으로 받아, 경로에 있는 동영상 bytes로 응답
    - 파일이 없으면 No File 204 응답
    - 브라우저에서 동영상을 재생하기 위한 필수값 Header에 전달 (content-type, content-length, accept-ranges)
    - HTTP requeest에 Range가 있으면 해당 부분부터 데이터 응답
    - Range가 없으면 처음부터 응답
    
    2. class RangeFileWrapper
    - 파일을 8kb 단위로 잘라서 iterable한 형태로 변환
    - 남은 용량이 없으면 파일 read중단
    
     커밋할 변경 사항:
    	수정함:        courses/tests.py
    	수정함:        courses/views.py
    	수정함:        ifLearn/settings.py
    	수정함:        ifLearn/urls.py
    
    피드백 반영(testcase)
    
    TEST CASE 수정
    
    컨플릭트 해결

--- courses/views.py
+++ courses/views.py
@@ -6,63 +6,7 @@ from wsgiref.util   import FileWrapper
 from django.http    import JsonResponse, StreamingHttpResponse
 from django.http.response import HttpResponse
 from django.views   import View 
-from courses.models import Category, Course,SubCategory, CourseHashtag
 
-
-class CategoryView(View) :
-    def get(self,request) :
-        subcategory_queryset= SubCategory.objects.values('id','name','category_id')
-        category_queryset   = Category.objects.values('name', 'id')
-        hashtag_queryset    = CourseHashtag.objects.select_related('hashtag','course').values('course__subcategory_id','hashtag__name','hashtag__id')
-        result              = []
-
-        for category in category_queryset :
-            result.append({
-            category["name"] : [{
-                "id" : sub["id"],
-                "subcategory" : sub["name"],
-                "hashtag" : [mid["hashtag__name"] for mid in hashtag_queryset if mid["course__subcategory_id"] == sub["id"]][:12]
-                }
-            for sub in subcategory_queryset if sub["category_id"] == category["id"]]
-            })
-
-        return JsonResponse({"reuslt" : result})
-
-class CourseView(View) :
-    def get(self,request) :
-        
-        course_queryset = Course.objects.select_related('level','subcategory', 'subcategory__category', 'sharer'
-        ).values('id','name','summary','thumbnail_url','price','level__name', 'subcategory__name', 'subcategory__category__name', 'sharer__nickname')
-        
-        hashtag_queryset    = CourseHashtag.objects.select_related('hashtag','course').values('course__id','hashtag__name','hashtag__id')
-
-
-        result          = []
-
-        for course in course_queryset :
-            result.append(
-            {
-                "id" : course["id"],
-                "thumbnail" : course["thumbnail_url"],
-                "title" : course["name"],
-                "author" : course["sharer__nickname"],
-                "price" : int(course["price"]),
-                "summary" : course["summary"],
-                "level" : course["level__name"],
-                "sub-cateogry" : course["subcategory__name"],
-                "category" : course["subcategory__category__name"],
-                "star-number" : 0,
-                "popular" : 0,
-                "hash-tag" : [
-                    {"tag_name": tag["hashtag__name"],
-                    "tag_id" : tag["hashtag__id"]
-                    } for tag in hashtag_queryset if tag["course__id"] == course["id"]
-                ]
-            })
-
-        return JsonResponse({
-            "result" : result
-        })
 
 class RangeFileWrapper:
     def __init__(self, file, blksize=8192, offset=0, length=None):
