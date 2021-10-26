10cafc5b504c68964036e35cef2ea293ddbf1ed4
KIM <km0192@naver.com>
2021-10-26 16:35:02 +0900

    테스트

--- courses/views.py
+++ courses/views.py
@@ -32,14 +32,13 @@ class CourseView(View) :
     def get(self,request) :
         
         course_queryset = Course.objects.select_related('level','subcategory', 'subcategory__category', 'sharer'
-        ).values('id','name','summary','thumbnail_url','price','level__name', 'subcategory__name', 'subcategory__category__name', 'sharer__nickname')
+        ).values('id','name','summary','thumbnail_url','price','level__name', 'subcategory__name', 'subcategory__category__name', 'sharer__nickname')[:20]
         
-        hashtag_queryset    = CourseHashtag.objects.select_related('hashtag','course').values('course__id','hashtag__name','hashtag__id')
+        course_ids = [c.id for c in course_queryset]
 
-
+        hashtag_queryset    = CourseHashtag.objects.select_related('hashtag','course').values('course__id','hashtag__name','hashtag__id').filter(course__id__in = course_ids)
         result          = []
-
-        for course in course_queryset :
+        for course in course_queryset[:20] :
             result.append(
             {
                 "id" : course["id"],
