7f44825a5e4f4c359111f2e20308ace9dbbfeb80
KIM <km0192@naver.com>
2021-10-28 14:35:06 +0900

    [ADD] Video page API View, Test
    	수정함:        courses/tests.py
    	수정함:        courses/urls.py
    	수정함:        courses/views.py

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,9 @@
 from django.urls    import path
-from courses.views  import VideoPlayer, CategoryListView
+from courses.views  import VideoPlayer, CategoryListView, VideoLayoutView, LectureDetail
 
 urlpatterns = [
     path('/categories', CategoryListView.as_view()),
-    path('/<path>', VideoPlayer.as_view()), #이것도 의논해야함
-    ]
+    path('/video/<int:course_id>', VideoLayoutView.as_view()),
+    path('/video/detail/<int:lecture_id>', LectureDetail.as_view()),
+    path('/<path>', VideoPlayer.as_view())
+]
